a4de3229c5d035485ee836daa7a3a22b72e853c3
bbnoirz <bbnoir83@gmail.com>
2023-04-22 15:53:07 +0800

    add clean_up

--- scripts/c_all.sh
+++ scripts/c_all.sh
@@ -1,7 +1,9 @@
 #ckeck pass and area: run 01 02; check pass and area
 cd ../01*
+./09*
 ./01*
 cd ../02*
+./09*
 ./01*
 echo ==========================================================================
 source ~/dotdcs/scripts/c_pass.sh
